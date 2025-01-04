3a54f7693884929ea93359c798d0c3558b6de046
--global <prabin20panta@gmail.com>
2025-01-04 22:09:28 +0545

    add: projects page and project fetch

---
+++ components/ProjectList.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+
+  <section v-if="pending">Loading...</section>
+  <section v-else-if="error">Loading...</section>
+  <section v-else>Here is data</section>
+</template>
+
+<script setup>
+const { error, pending, data } = await useFetch(
+  "https://api.github.com/users/andro-jr/repos"
+);
+</script>
