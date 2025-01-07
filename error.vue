0c5d33a1944657bce00a771b7ad9700500d5e4c1
--global <prabin20panta@gmail.com>
2025-01-07 14:15:06 +0545

    add: error handling

---
+++ error.vue
@@ -0,0 +1,14 @@
+<template>
+  <NuxtLayout>
+    <section>
+      <h1>OOPS! {{ error.status }}</h1>
+      <h2>{{ error.statusMessage }}</h2>
+    </section>
+  </NuxtLayout>
+</template>
+
+<script setup>
+defineProps({
+  error: Object,
+});
+</script>
--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,17 +1,24 @@
 <template>
   <article class="prose dark:prose-invert max-w-none">
-    <ContentDoc v-slot="{ doc }">
-      <div class="grid grid-cols-6 gap-16">
-        <div :class="doc.toc ? 'col-span-4' : 'col-span-6'">
-          <ContentRenderer :value="doc" />
+    <ContentDoc>
+      <template #not-found>
+        <div class="flex items-center flex-col w-full">
+          <h1 class="mt-10 mb-2">ERROR: 400</h1>
+          <h4 class="mt-0">Blog not found :(</h4>
         </div>
-        <div class="col-span-2 not-prose" v-if="doc.toc">
-          <aside class="sticky top-8">
-            <div class="font-semibold mb-2">Table of Contents</div>
-            <TOCLinks :links="doc.body.toc.links" :active-id="activeId" />
-          </aside>
-        </div>
-      </div>
+      </template>
+      <template v-slot="{ doc }">
+        <div class="grid grid-cols-6 gap-16">
+          <div :class="doc.toc ? 'col-span-4' : 'col-span-6'">
+            <ContentRenderer :value="doc" />
+          </div>
+          <div class="col-span-2 not-prose" v-if="doc.toc">
+            <aside class="sticky top-8">
+              <div class="font-semibold mb-2">Table of Contents</div>
+              <TOCLinks :links="doc.body.toc.links" :active-id="activeId" />
+            </aside>
+          </div></div
+      ></template>
     </ContentDoc>
   </article>
 </template>
